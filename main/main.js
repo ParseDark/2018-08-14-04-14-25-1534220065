800dfc5b7fe55500c644e50ac757099cb056b391
PaserDark <whd1073363531@qq.com>
2019-05-27 19:12:59 +0800

    LCD灯

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    let charMap = getCharMap();
+
+    const strArr = converArray(str.split('')
+                    .map(item => {
+                        return charMap[item]
+                    }))
+                    .map(item => `${item.join(' ')}`)
+                    .join('\n');
+    return `${strArr}\n`;
 };
+
+// 从题目获取数字对应的字符串
+const getCharMap = () => {
+    const charStr = `._.   ...   ._.   ._.   ...   ._.   ._.   ._.   ._.   ._.
+    |.|   ..|   ._|   ._|   |_|   |_.   |_.   ..|   |_|   |_|
+    |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|`
+    let charMap = {};
+    const arr = charStr.replace(/\s/g, "###").split("###").filter(item => item != "");
+    let i = 0;
+    while (i < 10) {
+        charMap[i] = [arr[i], arr[i + 10], arr[i + 20]];
+        i++;
+    }
+    return charMap;
+}
+
+// 矩阵转换
+const converArray = arr => {
+    return arr[0].map(function (col, i) {
+        return arr.map(function (row) {
+            return row[i];
+        })
+    });
+}
